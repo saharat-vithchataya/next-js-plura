05a4917e56c645137fc9f46e9efa8cd70964a8b8
Saharat Vithchataya <vithchataya.saharat@gmail.com>
2024-02-29 14:59:24 +0700

    update sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "@/providers/theme-provider";
+import ModalProvider from "@/providers/modal-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +27,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <ModalProvider>
+            {children} <Toaster />
+          </ModalProvider>
         </ThemeProvider>
       </body>
     </html>
